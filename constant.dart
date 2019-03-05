374a92be8efbdce37614022aa520afec07e584ee
sandunsameera <sandunsameera25@gmail.com>
2019-03-05 12:44:43 +0530

    use of final and const keyword in dart

---
+++ constant.dart
@@ -0,0 +1,26 @@
+void main(){
+
+  //final keyworf
+
+  final city = "Weligama";
+  final country = "SriLamka";
+
+
+  //const keyword
+
+  const PI = 3.14;
+
+
+}
+
+class circle{
+
+  //final keyword can be used freely in classes
+  final color = "red";
+
+  //const keyword need static keyword to be used in instance classes
+
+  static const PI = 3.14;
+
+
+}
