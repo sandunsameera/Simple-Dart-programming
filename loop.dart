cbdf855e7e269668f7388637d645a0da04328841
sandunsameera <sandunsameera25@gmail.com>
2019-03-05 17:20:55 +0530

    loops in dart

---
+++ loop.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  var i =0;
+  //for loop
+
+  for(var i =0;i<10;i++){
+    print("This is $i  time");
+  }
+
+  //while loop
+  while( i <5){
+
+    print("This  is while loop");
+    i++;
+
+  }
+  print("");
+
+  //do while loop
+  do{
+    print("This is do while");
+    i++;
+  }while(i<5);
+
+}
