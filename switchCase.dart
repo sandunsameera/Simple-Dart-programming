f0c4463e6cb6006f0f7f4f67e495073d3a232ec3
sandunsameera <sandunsameera25@gmail.com>
2019-03-05 13:31:28 +0530

    use of switch case statement in dart

---
+++ switchCase.dart
@@ -0,0 +1,15 @@
+void main(){
+  String grade = "A";
+  switch(grade){
+    case "A":
+      print("Excellent grade");
+      break;
+
+    case "B":
+      print("Good grade");
+      break;
+
+    default:
+      print("Invalid grade");
+  }
+}
