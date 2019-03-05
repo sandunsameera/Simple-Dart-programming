6981da49736064facc65941539084df787eca2c8
sandunsameera <sandunsameera25@gmail.com>
2019-03-05 18:51:52 +0530

    functions in dart

--- functions.dart
+++ functions.dart
@@ -5,6 +5,8 @@ void main(){
   int result = area(10, 20);
   print("Area of rectangle is $result");
 
+  print("Area is ${area1(10, 20)}");
+
 
 
 }
@@ -25,3 +27,7 @@ int area (leanth,breath){
     int ar = leanth*breath;
     return ar;
 }
+
+//short hand functions
+
+int area1  (leanth,brath) => leanth*brath;
