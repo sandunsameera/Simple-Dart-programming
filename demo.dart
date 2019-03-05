771742aa8871406ce3a326e0599353cffb11c229
sandunsameera <sandunsameera25@gmail.com>
2019-03-05 12:31:26 +0530

    String laterals and string interpolation using dart

--- demo.dart
+++ demo.dart
@@ -1,3 +1,22 @@
 void main(){
-  print (3); 
+
+  String s1 = 'single';
+  String s2 = "double";
+
+  print (s1);
+  print (s2);
+
+  //String interpolation
+
+  String name = "sandun";
+
+  print("My name is $name");
+
+
+  int l = 20;
+  int m = 10;
+
+  print("Area of leanth $l and breath $m is equal to ${l*m}");
+
+
 }
