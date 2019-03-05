6861b5d2d00bcf33d95a9fed11d4601a5b7ee851
sandunsameera <sandunsameera25@gmail.com>
2019-03-05 12:53:41 +0530

    use of if else and else if conditions in dart

---
+++ conditional.dart
@@ -0,0 +1,20 @@
+void main(){
+
+  //if else conditional statement
+  var salary = 2500;
+  if(salary < 10000)
+    print("Good salary");
+  else
+    print("bad salary");
+
+  //If else if
+
+  var marks = 60;
+  if(marks <30)
+    print("you got s pass");
+  else if(marks < 50)
+    print("you got c pass");
+  else
+    print("you got B pass");
+
+}
