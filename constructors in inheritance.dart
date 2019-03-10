5e7c77847a53dc83839ad14e92cd15d987f90c73
sandunsameera <sandunsameera25@gmail.com>
2019-03-10 21:04:07 +0530

    constructor in inheritance

---
+++ constructors in inheritance.dart
@@ -0,0 +1,19 @@
+void main(){
+
+}
+
+class Animal{
+  String color;
+
+  Animal(){
+    print("Animal class constructor");
+  }
+}
+
+class Dog extends Animal{
+  String breed;
+
+  Dog(){
+    print("Dog class comstructor");
+  }
+}
