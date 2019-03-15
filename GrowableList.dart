960cb8d58f1aefa82269869c8ce6d2506bcd9246
sandunsameera <sandunsameera25@gmail.com>
2019-03-15 22:10:19 +0530

    use of lambda functions in dart

--- GrowableList.dart
+++ GrowableList.dart
@@ -1,3 +1,23 @@
 void main(){
 
+  List<int> numberList = List(); //growable list
+
+  numberList[0] = 55;
+  numberList[1] = 65;
+  numberList[2] = 75;
+  numberList[3] = 85;
+  numberList[4] = 95;
+
+  //All are supported in not supported in fixed-length list
+
+  numberList[0] = 99; // you can update values
+  numberList[1] = null;//delete values
+
+  numberList.removeLast(); //remove last value of list
+  numberList.remove(85); //remove using value
+  numberList.removeAt(1); //remove using index
+
+  numberList.add(66); //add value
+
+
 }
